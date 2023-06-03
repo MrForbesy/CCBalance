a3272a67efc8ff284b156db68efe665933bcd2d1
Forbesy <mvforbes21@gmail.com>
2023-06-02 23:09:11 -0400

    Completed code

--- src/MonthlyCCBalance.java
+++ src/MonthlyCCBalance.java
@@ -1,5 +1,13 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MonthlyCCBalance
+{
+    public static void main(String[] args)
+    {
+      int startBalance = 5000;
+      double interestRate = .17;
+      double oneMonth = startBalance + (startBalance * interestRate);
+      double twoMonth = oneMonth + (oneMonth * interestRate);
+
+        System.out.println("After one month, your payment on the starting balance of " + startBalance + " is " + oneMonth + ".");
+        System.out.println("After two months, your payment is now " + twoMonth + ".");
     }
 }
